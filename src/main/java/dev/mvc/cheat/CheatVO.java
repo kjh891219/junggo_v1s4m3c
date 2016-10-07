23f96a50ffc11fbb7d834389be2052b151e81df7
iamsure21c <besure21c@naver.com>
2016-10-07 17:19:51 +0900

    aa

--- src/main/java/dev/mvc/cheat/CheatVO.java
+++ src/main/java/dev/mvc/cheat/CheatVO.java
@@ -1,5 +1,5 @@
 package dev.mvc.cheat;
-
+   
 public class CheatVO {
   private int ctno;
   private String gubun;
@@ -49,7 +49,7 @@ public class CheatVO {
   }
   public String getCheatid() {
     return cheatid;
-  }
+  }  
   public void setCheatid(String cheatid) {
     this.cheatid = cheatid;
   }
